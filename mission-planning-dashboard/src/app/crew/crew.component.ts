c39a728a830e5d755c92cf4a1ed74c0005323af6
lylej312 <lyle.jensen95@gmail.com>
2021-04-10 21:35:30 -0700

    Completed mission planner studio 1

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css'],
 })
 export class CrewComponent implements OnInit {
-  crew: string[] = ['Jessica Watkins', 'Raja Chari', 'Jasmin Moghbeli'];
+  crew: string[] = [
+    'Jessica Watkins',
+    'Raja Chari',
+    'Jasmin Moghbeli',
+    'Celiana King',
+  ];
   constructor() {}
 
   ngOnInit(): void {}
